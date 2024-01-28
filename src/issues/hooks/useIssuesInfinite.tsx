6d3cbe8fc9198216d5cb4e43d5a1547e25f6557c
Carlos Zabala <czabala847@gmail.com>
2024-01-27 20:26:05 -0500

    infinite scroll

---
+++ src/issues/hooks/useIssuesInfinite.tsx
@@ -0,0 +1,60 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githubApi";
+import { sleep } from "../../helpers";
+import { Issue, State } from "../interfaces";
+interface PropsUseIssues {
+  state?: State;
+  labels: string[];
+  page?: number;
+}
+
+interface QueryProps {
+  pageParam?: number;
+  queryKey: (string | PropsUseIssues)[];
+}
+
+const getIssues = async ({
+  pageParam = 1,
+  queryKey,
+}: QueryProps): Promise<Issue[]> => {
+  try {
+    const [, , args] = queryKey;
+    const { state, labels } = args as PropsUseIssues;
+
+    await sleep(2);
+
+    const params = new URLSearchParams();
+
+    if (state) params.append("state", state);
+
+    if (labels.length > 0) {
+      const labelString = labels.join(",");
+      params.append("labels", labelString);
+    }
+
+    params.append("page", pageParam.toString());
+    params.append("per_page", "5");
+
+    const { data } = await githubApi.get<Issue[]>("/issues", { params });
+    return data;
+  } catch (error) {
+    return [];
+  }
+};
+
+export const useIssuesInfinite = ({ state, labels }: PropsUseIssues) => {
+  const issuesQuery = useInfiniteQuery({
+    queryKey: ["issues", "infinite", { state, labels }],
+    queryFn: (data) => getIssues(data),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.length === 0) return;
+
+      return pages.length + 1;
+    },
+  });
+
+  return {
+    issuesQuery,
+  };
+};
